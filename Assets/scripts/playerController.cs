f91b7e6ca6c03f1af8a7dc02d5685b57229b77ad
umsomi70 <111692216+umsomi70@users.noreply.github.com>
2023-08-18 09:18:12 +0200

    Other Plane Prefabs
    I have the concrete and the tree prefabs. The Player also has a trigger for lives. The level generation is on another script.

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -9,21 +9,14 @@ public class playerController : MonoBehaviour
     [SerializeField] private float maxtravel = 2.5f;
     [SerializeField] private float sideSpeed;
 
-    [SerializeField] private float runSpeed = 3;
 
     [SerializeField] private float jumpHeight = 2;
     [SerializeField] private float jumpTime = 2;
     bool upwards = true;
     bool isJumping = false;
 
-    [Header("Level Movement")]
-    [SerializeField] private GameObject currentSection;
-    [SerializeField] private GameObject nextSection;
+    int hp = 2;
 
-    [SerializeField] private Transform sectionSpawn;
-    [SerializeField] private GameObject sectionPref;
-
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -33,23 +26,7 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //This code controls the movement of the floor.
-        //It's supposed to be expanded upon
-        //Contact me if you need help with it.
         
-        if(nextSection == null){
-            nextSection = Instantiate(sectionPref, sectionSpawn);
-        }else if(currentSection.transform.position.x < -26){
-            Destroy(currentSection);
-            currentSection = nextSection;
-            nextSection = Instantiate(sectionPref, sectionSpawn);
-        }else{
-            currentSection.transform.position = new Vector3(currentSection.transform.position.x - (runSpeed * Time.deltaTime), currentSection.transform.position.y, currentSection.transform.position.z);  
-            nextSection.transform.position = new Vector3(nextSection.transform.position.x - (runSpeed * Time.deltaTime), nextSection.transform.position.y, nextSection.transform.position.z);   
- 
-        }
-
-
 
         if(Input.GetKey(KeyCode.S)){
             duck();
@@ -104,5 +81,13 @@ public class playerController : MonoBehaviour
 
         yield return new WaitForSeconds(jumpTime/2);
         isJumping = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Level"))
+        {
+            hp = 0;
+        }
     }
 }
